c4f3268edbccd3786582b7b84bb931e27663e3f2
david <davidescu.farcas@gmail.com>
2025-12-05 22:38:06 +0200

    day 5 done

--- day5.cpp
+++ day5.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include <sstream>
 using namespace std;
 ifstream file ("date.in");
 vector<pair<long long,long long>> range;
@@ -34,9 +33,36 @@ int solve1() {
         }
     }
     return count;
+}
+bool canunite(pair<long long,long long> a,pair<long long,long long> b) {
+    return b.first<=a.second;
+}
+long long solve2() {
+    long long count=0;
+    read();
+    sort(range.begin(),range.end(),[](pair<long long,long long> a,pair<long long,long long> b) {
+        if (a.first!=b.first)
+            return a.first>b.first;
+        return a.second>b.second;
+    });
+    while(range.size()>1) {
+        auto e1=range.back();
+        range.pop_back();
+        if(canunite(e1,range.back())){
+            auto e2=range.back();
+            range.pop_back();
+            range.push_back(make_pair(e1.first,max(e1.second,e2.second)));
+        }
+        else {
+            count+=e1.second-e1.first+1;
+        }
+    }
+    count+=range.back().second-range.back().first+1;///forgot this
+    return count;
 }///ans:525
 int main() {
-    read();
-    cout<<solve1();
+    ///read();
+    ///cout<<solve1()<<endl; answer is 525 for part 1
+    cout<<solve2()<<endl;///answer is 333892124923577
     return 0;
 }
