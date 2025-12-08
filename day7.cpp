63096f183122ee85e36b3cc0a6e9d90987ea1cff
den1ed <davidescu.farcas@gmail.com>
2025-12-08 08:50:18 +0200

    added part2 finally
    Implemented a new function 'solve2' to calculate paths in a grid and updated the main function to call 'solve2' instead of 'solve1'.

--- day7.cpp
+++ day7.cpp
@@ -8,6 +8,7 @@ ifstream file ("input.txt");
 vector<string> m;
 string line;
 vector<bitset<150>> visited;
+vector<vector<long long>> dp;
 int nrs=0;
 void read() {
     while (getline(file, line)) {
@@ -48,9 +49,33 @@ void solve1() {
         }
     }
 }///ans:1635
+void solve2(){
+    int n=m.size();
+    int mm=m[0].size();
+    dp=vector<vector<long long>>(n+1,vector<long long>(mm+1));
+    for (int i=0; i<mm; i++){
+        dp[n][i]=1;
+    }
+    for (int i=n-1; i>=0; i--){
+        for(int j=0; j<mm; j++){
+            if (m[i][j]=='.'||m[i][j]=='S'){
+                dp[i][j]=dp[i+1][j];
+            }
+            else {
+                long long left,right;
+                left=(j>0 ? dp[i+1][j-1] :0);
+                right=(j<mm ? dp[i+1][j+1] : 0);
+                dp[i][j]=left+right;
+            }
+        }
+    }
+}///ans:58097428661390
 int main() {
     read();
-    solve1();
-    cout<<nrs;
+    ///solve1();
+    ///cout<<nrs;
+    solve2();
+    int s=m[0].find('S');
+    cout<<dp[0][s];
     return 0;
 }
