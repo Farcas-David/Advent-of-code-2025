89869a4312add4007e7be057559255bbf8e1ffa0
david <davidescu.farcas@gmail.com>
2025-12-11 16:45:50 +0200

    finally decided to make a header file cuz it got tired of writing it everytime

---
+++ libaoc.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
